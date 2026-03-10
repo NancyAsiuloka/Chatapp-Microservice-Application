78956d85e760c4a931751af75ace0f30b056f6a6
NancyAsiuloka <nancyasiuloka@gmail.com>
2026-03-10 15:30:27 +0100

    feat: added error handler middleware for auth-service

--- services/auth-service/src/middleware/error-handler.ts
+++ services/auth-service/src/middleware/error-handler.ts
@@ -6,5 +6,11 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
     logger.error({ err }, "Unhandled error occured");
     const error = err instanceof HttpError ? err : undefined;
 
-    
+    const statusCode = error?.statusCode ?? 500;
+    const message = statusCode >= 500 ? "Internal server error": error?.message ?? "Unknown Error";
+    const payload = error?.details ? { message, details: error.details } : { message }
+
+    res.status(statusCode).json(payload);
+
+    void _next()
 }
