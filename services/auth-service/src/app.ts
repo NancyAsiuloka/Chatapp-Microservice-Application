a033f36227bef21c2a753994b4ad96ad00cea795
NancyAsiuloka <nancyasiuloka@gmail.com>
2026-03-10 15:22:55 +0100

    feat: integrate cors and helmet middleware in auth service

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -1,7 +1,25 @@
 import express, { type Application } from "express"
+import cors from "cors";
+import helmet from "helmet"
 
 export const createApp = () : Application => {
     const app =  express();
 
+    app.use(helmet())
+    app.use(
+        cors({
+            origin: "*",
+            credentials: true
+        })
+    )
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true}))
+
+
+    app.use((_req, res) => {
+        res.status(400).json({ message: "Not found"})
+    })
+
+    
     return app
 }
