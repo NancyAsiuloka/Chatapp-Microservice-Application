5a67a171941561d9cbace2b0c32ab5715065c800
NancyAsiuloka <nancyasiuloka@gmail.com>
2026-04-08 00:53:19 +0100

    Updated `index.ts` to export new event types and modified env for RabbitMQ configuration.

--- services/auth-service/src/config/env.ts
+++ services/auth-service/src/config/env.ts
@@ -10,8 +10,8 @@ const envSchema = z.object({
   JWT_EXPIRES_IN: z.string().default('1d'),
   JWT_REFRESH_SECRET: z.string().min(32),
   JWT_REFRESH_EXPIRES_IN: z.string().default('30d'),
-  // RABBITMQ_URL: z.string(),
-  INTERNAL_AUTH_TOKEN: z.string().min(32),
+  RABBITMQ_URL: z.string(),
+  INTERNAL_API_TOKEN: z.string().min(32),
 });
 
 type EnvType = z.infer<typeof envSchema>;
