3c8e9994414d582a3e0918e43952ad3b8c2af49a
NancyAsiuloka <nancyasiuloka@gmail.com>
2026-04-08 00:54:16 +0100

    Refactored internal auth middleware token variable and created event publishing logic for user registration.

--- packages/common/dist/index.js
+++ packages/common/dist/index.js
@@ -5,4 +5,7 @@ export * from "./errors/http-error";
 export * from "./http/async-handler";
 export * from "./http/validate-request";
 export * from "./http/internal-auth";
+export * from './events/event-types';
+// export * from './events/user-events';
+export * from './events/auth.event';
 //# sourceMappingURL=index.js.map
