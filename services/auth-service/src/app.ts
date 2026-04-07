8b9312c087a2f547780765280c95c3db60a2019b
NancyAsiuloka <nancyasiuloka@gmail.com>
2026-04-08 00:55:55 +0100

    Implemented error handling middleware ad added logger utility for user service.

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -18,7 +18,7 @@ export const createApp = () : Application => {
     )
     app.use(express.json())
     app.use(express.urlencoded({ extended: true}))
-    app.use(createInternalAuthMiddleware(env.INTERNAL_AUTH_TOKEN))
+    app.use(createInternalAuthMiddleware(env.INTERNAL_API_TOKEN))
 
     registerRoutes(app)
 
