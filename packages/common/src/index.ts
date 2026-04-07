ac6b1fe41eeca9d103948fb496e48994896f40b6
NancyAsiuloka <nancyasiuloka@gmail.com>
2026-04-08 00:56:06 +0100

    Configured TypeScript settings for user service.

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -6,3 +6,6 @@ export * from "./errors/http-error"
 export * from "./http/async-handler"
 export * from "./http/validate-request"
 export * from "./http/internal-auth"
+export * from './events/event-types';
+// export * from './events/user-events';
+export * from './events/auth.event';
